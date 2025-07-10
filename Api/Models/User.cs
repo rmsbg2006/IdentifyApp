8ade9ddb5239fc353848534c753e64bad9db5edd
rmsbg2006 <rmsbg2006@yahoo.com>
2025-07-10 10:10:32 -0700

    02 Api Project Set AspNetCore Identity Added

---
+++ Api/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace Api.Models
+{
+    public class User : IdentityUser
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ Api/Services/JWTService.cs
@@ -0,0 +1,45 @@
+﻿using Api.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Api.Services
+{
+    public class JWTService
+    {
+        private readonly IConfiguration _config;
+        public JWTService(IConfiguration config, UserManager<User> userManager)
+        {
+            _config = config;
+        }
+        public string CreateJWT(User user)
+        {
+            var userClaims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email, user.UserName),
+                new Claim(ClaimTypes.GivenName, user.FirstName),
+                new Claim(ClaimTypes.Surname, user.LastName),
+            };
+
+            var key = Encoding.ASCII.GetBytes(_config["JWT:Key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(userClaims),
+                Expires = DateTime.UtcNow.AddDays(int.Parse(_config["JWT:ExpiresInDays"])),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Issuer = _config["JWT:Issuer"]
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
